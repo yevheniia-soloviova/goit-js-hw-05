1db54a502e58f73aa5b190cd08dc3fc2f4b45ed7
Yevheniia Soloviova <65976186+yevheniia-soloviova@users.noreply.github.com>
2020-09-05 19:21:58 +0300

    Create task-2.js

---
+++ js/task-2.js
@@ -0,0 +1,25 @@
+const calculateEngravingPrice = (message = "", pricePerWord = 0) =>
+  message.split(" ").length * pricePerWord;
+console.log(
+  calculateEngravingPrice(
+    "Proin sociis natoque et magnis parturient montes mus",
+    10
+  )
+); // 80
+
+console.log(
+  calculateEngravingPrice(
+    "Proin sociis natoque et magnis parturient montes mus",
+    20
+  )
+); // 160
+
+console.log(
+  calculateEngravingPrice("Donec orci lectus aliquam est magnis", 40)
+); // 240
+
+console.log(
+  calculateEngravingPrice("Donec orci lectus aliquam est magnis", 20)
+); // 120
+
+console.log(calculateEngravingPrice("Uno", 100)); // 100
