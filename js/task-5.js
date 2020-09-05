fda5a4a9e20ba44fe45e5795864c63da42270884
Yevheniia Soloviova <65976186+yevheniia-soloviova@users.noreply.github.com>
2020-09-05 20:29:29 +0300

    Update task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -1,18 +1,30 @@
-function checkForSpam(str) {
-  if (
-    str.toLowerCase().includes("spam") ||
-    str.toLowerCase().includes("sale")
-  ) {
-    return true;
-  } else {
-    return false;
+function getAllPropValues(array, prop) {
+  "use strict";
+  // Write code under this line
+  const result = [];
+  for (const key of array) {
+    if (prop in key) {
+      result.push(key[prop]);
+    }
   }
+  return result;
 }
+// Объекты и ожидаемый результат
+const products = [
+  { name: "Радар", price: 1300, quantity: 4 },
+  { name: "Радар", price: 1280, quantity: 2 },
+  { name: "Радар", price: 1320, quantity: 1 },
+  { name: "Сканер", price: 2700, quantity: 1 },
+  { name: "Сканер", price: 2500, quantity: 3 },
+  { name: "Дроид", price: 400, quantity: 7 },
+  { name: "Захват", price: 1200, quantity: 2 },
+];
 
-console.log(checkForSpam("Latest technology news")); // false
+console.log(getAllPropValues(products, "name"));
+// ['Радар', 'Радар', 'Радар', 'Сканер', 'Сканер', 'Дроид', 'Захват']
 
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(getAllPropValues(products, "quantity"));
+// [4, 2, 1, 1, 3, 7, 2]
 
-console.log(checkForSpam("Get best sale offers now!")); // true
-
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+console.log(getAllPropValues(products, "category"));
+//  []
