25f1d07758fcc4f46d31bb3004bca9c720853075
Yevheniia Soloviova <65976186+yevheniia-soloviova@users.noreply.github.com>
2020-09-12 17:09:29 +0300

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,29 +1,23 @@
-let message = "";
-const user = {
-  age: 20,
-  hobby: "html",
-  name: "Mango",
-  premium: true,
-};
-
-//Write code under this line
-user.mood = "happy";
-user["full time"] = true;
-user.hobby = "skydiving";
-user.premium = false;
-
-const keys = Object.keys(user);
+const addIndex = (element, index) => element + index;
 // Write code under this line
-for (const key of keys) {
-  message += `${key} : ${user[key]}\n`;
+const subIndex = (element, index) => element - index;
+
+function mapArray(array, cb) {
+  "use strict";
+  // Write code under this line
+  const numbers = new Array(array.length);
+  for (let i = 0; i < array.length; i += 1) {
+    const element = array[i];
+    const index = i;
+    numbers[i] = cb(element, index);
+  }
+  return numbers;
 }
 
-console.log(message);
-/*
-'age : 20
-hobby : skydiving
-name : Mango
-premium : false
-mood : happy
-"full time" : true
-' */
+const arr = [1, 2, 3, 4, 5];
+
+console.log(mapArray(arr, addIndex));
+// [1, 3, 5, 7, 9]
+
+console.log(mapArray(arr, subIndex));
+// [1, 1, 1, 1, 1]
