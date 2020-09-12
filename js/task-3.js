ea0fe96dbadbac8e623d5c247a3e1f90a8de60b0
Yevheniia Soloviova <65976186+yevheniia-soloviova@users.noreply.github.com>
2020-09-12 17:25:59 +0300

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,40 +1,34 @@
-const findBestEmployee = function (employees) {
+const add = (accum, element) => accum + element;
+const mult = (accum, element) => accum * element;
+const sub = (accum, element) => accum - element;
+
+function reduceArray(array, cb, initial) {
   "use strict";
-  // Write code under this line
-  const keys = Object.keys(employees);
-  let result = "";
-  let maxValue = 0;
-  for (const key of keys) {
-    if (employees[key] > maxValue) {
-      maxValue = employees[key];
-      result = key;
-    }
+  let i;
+  let accum;
+  if (arguments.length >= 3) {
+    accum = initial;
+    i = 0;
   }
-  return result;
-};
+  if (arguments.length === 2) {
+    accum = array[0];
+    i = 1;
+  }
+  for (i; i < array.length; i += 1) {
+    const element = array[i];
+    // Write code under this line
+    accum = cb(accum, array[i]);
+  }
+  return accum;
+}
 
-// Объекты и ожидаемый результат
-const developers = {
-  ann: 29,
-  david: 35,
-  helen: 1,
-  lorence: 99,
-};
-console.log(findBestEmployee(developers));
-// 'lorence'
+const arr = [1, 2, 3, 4, 5];
 
-const supports = {
-  poly: 12,
-  mango: 17,
-  ajax: 4,
-};
-console.log(findBestEmployee(supports));
-// 'mango'
+console.log(reduceArray(arr, add)); // 15
+console.log(reduceArray(arr, add, 10)); // 25
 
-const sellers = {
-  lux: 147,
-  david: 21,
-  kiwi: 19,
-  chelsy: 38,
-};
-console.log(findBestEmployee(sellers));
+console.log(reduceArray(arr, mult)); // 120
+console.log(reduceArray(arr, mult, 10)); // 1200
+
+console.log(reduceArray(arr, sub)); // -13
+console.log(reduceArray(arr, sub, 10)); // -5
