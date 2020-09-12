7131d223604e2f2e6cea6eca7a773798fff6710d
Yevheniia Soloviova <65976186+yevheniia-soloviova@users.noreply.github.com>
2020-09-12 17:26:02 +0300

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,27 +1,33 @@
-const countTotalSalary = function (employees) {
-  "use strict";
-  // Write code under this line
-  const values = Object.values(employees);
-  let total = 0;
-  for (const value of values) {
-    total += value;
-  }
-  return total;
+const account = {
+  owner: "Mango",
+  balance: 24000,
+  discount: 0.1,
+  orders: ["order-1", "order-2", "order-3"],
+  changeDiscount(value) {
+    this.discount = value; // Write code in this line
+  },
+  showOrders() {
+    return this.orders; // Write code in this line
+  },
+  addOrder(cost, order) {
+    this.balance -= cost; // Write code in this line
+    this.orders.push(order); // Write code in this line
+  },
 };
+const copyAccount = Object.assign({}, account);
+copyAccount.orders = [...account.orders];
+// копируем для автотестов ссылочные типы
 
-// Объекты и ожидаемый результат
-const developers = {
-  mango: 300,
-  poly: 250,
-  alfred: 450,
-};
-console.log(countTotalSalary(developers));
-// 1000
+/*
+account.changeDiscount(0.15);
+console.log(account.discount); // 0.15
 
-const supports = {
-  kiwi: 200,
-  lux: 150,
-  chelsy: 150,
-};
-console.log(countTotalSalary(supports));
-// 500
+console.log(account.showOrders());
+// ['order-1', 'order-2', 'order-3']
+
+account.addOrder(5000, 'order-4');
+console.log(account.balance); // 19000
+
+console.log(account.showOrders());
+// ['order-1', 'order-2', 'order-3', 'order-4']
+*/
